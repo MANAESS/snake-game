5fab97cade465ba287c9b231a7836b7fc56e4274
MANAESS <moudal200244@gmail.com>
2026-07-21 12:02:41 +0000

    Add renderer and background color rendering

--- snake.c
+++ snake.c
@@ -5,8 +5,8 @@ int main() {
     printf("Hello Snake\n");
 
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_CreateWindow("Classic Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-
+    SDL_Window *window = SDL_CreateWindow("Classic Snake", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
     int running = 1;
     while (running) {
         SDL_Event event;
@@ -14,10 +14,19 @@ int main() {
             if (event.type == SDL_QUIT) {
                 running = 0;
             }
-        }
-        SDL_Delay(100);
     }
+    
+    SDL_SetRenderDrawColor(renderer,0,255,0,255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 
+    SDL_Delay(100);
+    }
+    
+  
+
+    
     SDL_Quit();
     return 0;
 }
+
